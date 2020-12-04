9a6d7ccc6ac1b2f6919fca272691350b0d36859c
Juliancito1 <julianser@hotmail.com.ar>
2020-12-04 16:53:27 -0300

    Avance

--- Modulo de Administración.cpp
+++ Modulo de Administración.cpp
@@ -6,9 +6,9 @@
 struct veterinario
 {
 	char ApeNom[60];
-	int matri;
+	int matricula;
 	int dni;
-	char tel[25];
+	char telefono[25];
 };
 
 struct usuario
@@ -66,20 +66,19 @@ void registrarvet(FILE *vet1, veterinario vet)
 	_flushall();
 	gets(vet.ApeNom);
 	printf("\nIngrese su matricula: ");
-	scanf("%d",&vet.matri);
+	scanf("%d",&vet.matricula);
 	printf("\nIngrese su dni: ");
 	scanf("%d",&vet.dni);
-	printf("\nIngrese su nï¿½mero de telï¿½fono: ");
+	printf("\nIngrese su numero de telefono: ");
 	_flushall();
-	gets(vet.tel);
+	gets(vet.telefono);
 	fwrite(&vet,sizeof(veterinario),1,vet1);
 	fclose(vet1);
 }
 
 void registrarusuario(FILE *usuario1, usuario user)
 {
-	int i,v=0;
-	char letras[28]= "abcdefghijklmnï¿½opqrstuvwxyz";
+	int i,v=0,bandera=0,mayus=0,num=0;
 	system("CLS");
 	printf("\tIngrese los datos del usuario");
 	printf("\nNombre de Usuario: ");
@@ -90,35 +89,49 @@ void registrarusuario(FILE *usuario1, usuario user)
 		if(user.usuario[0]>96 and user.usuario[0]<123)
 		{
 			printf("\n La primera letra es minuscula.");
+			i++;
+			if(user.usuario[i]>=65 and user.usuario[i]<=90)
+			{
+				mayus++;
+				if(mayus>=2)
+				{
+					printf("\nSe ingresaron al menos 2 letras mayúsculas.");
+					if(user.usuario[i]>=48 and user.usuario[i]<=57)
+					{
+						num++;
+						if(num==3)
+						{
+							printf("\nEl usuario tiene 3 digitos");
+							printf("\n\nUsuario Aprobado");
+							bandera=1;
+						}
+					}
+				}
+			}
 			//v++;
 		}
-		i++;
-		/*if(v==0)
+		else
 		{
-			printf("Error usuario no vï¿½lido - El primer carï¿½cter debe ser minï¿½scula Ingrese nuevamente.");
-			system("pause");
-			system("CLS");
-			printf("\nNombre de Usuario: ");
-			_flushall();
-			gets(user.usuario);
-		}*/
+			printf("\nUsuario invalido");
+		}
+		
 	}
-	while(strlen(user.usuario)>=6 and strlen(user.usuario)<=10);
+	while(strlen(user.usuario)>=6 and strlen(user.usuario)<=10 and bandera==1);
 }
 
 int menuprincipal()
 {
 	int opc=0;
 	system("CLS");
-	printf("\tMï¿½dulo Administraciï¿½n");
+	printf("\tModulo Administracion");
 	printf("\n======================");
 	printf("\n1.- Registrar Veterinario");
 	printf("\n2.- Registrar Usuario Asistente");
 	printf("\n3.- Atenciones por Veterinarios");
 	printf("\n4.- Ranking de Veterinarios por atenciones");
 	printf("\n5.- Registrar Veterinario");
-	printf("\n6.- Cerrar Aplicaciï¿½n");
-	printf("\nSeleccione una opciï¿½n: ");
+	printf("\n6.- Cerrar Aplicacion");
+	printf("\nSeleccione una opcion: ");
 	scanf("%d",&opc);
 	return opc;
 }

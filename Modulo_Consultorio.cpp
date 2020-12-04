aad28932d8d600cd00671e229a6c9357b81fa4d0
JeremiasIvanoff <jeremias.morenoivanoff@alu.frt.utn.edu.ar>
2020-12-04 19:08:18 -0300

    función cargar_registros listas pero no probada

--- Modulo_Consultorio.cpp
+++ Modulo_Consultorio.cpp
@@ -57,6 +57,7 @@ main()
     Mascota reg_mascotas[50];
     Turnos reg_turnos[50];
     int num_usuarios = 0,num_vets = 0,num_mascotas = 0,num_turnos = 0;
+    
     cargar_registros(reg_usuarios,reg_vets,reg_mascotas,reg_turnos,num_usuarios,num_vets,num_mascotas,num_turnos);
 	
 	end();
@@ -66,19 +67,86 @@ void cargar_registros(Usuarios &reg_usuarios[50],Veterinario &reg_vets[50],Masco
 {
     FILE *arch;
     int i = 0;
-     
-    arch=fopen("Usuarios.dat","rb");
-
-    fread(&reg_usuarios[i],sizeof(Usuarios),1,arch);
-    while (!feof(arch))
-    {
-       i++;
-       fread(&reg_usuarios[i],sizeof(Usuarios),1,arch); 
-    }
-    num_usuarios = i;
-    i = 0;
-
     
+    //Archivo de usuarios:
+    arch=fopen("Usuarios.dat","rb");
+    
+    if (arch==NULL)
+    {
+        printf("\nEl archivo no fue creado o se elimino, contacte con soporte.");
+    }
+    else
+    {
+        fread(&reg_usuarios[i],sizeof(Usuarios),1,arch);
+        while (!feof(arch))
+        {
+        i++;
+        fread(&reg_usuarios[i],sizeof(Usuarios),1,arch); 
+        }
+        num_usuarios = i;
+        i = 0;
+        fclose(arch);
+    }
+    
+    //Archivo de veterinarios:
+    arch=fopen("Veterinarios.dat","rb");
+
+    if (arch==NULL)
+    {
+        printf("\nEl archivo no fue creado o se elimino, contacte con soporte.");
+    }
+    else
+    {
+        fread(&reg_vets[i],sizeof(Veterinario),1,arch);
+        while (!feof(arch))
+        {
+        i++;
+        fread(&reg_vets[i],sizeof(Veterinario),1,arch); 
+        }
+        num_vets = i;
+        i = 0;
+        fclose(arch);
+    }
+    
+    //Archivo de mascotas:
+    arch=fopen("Mascotas.dat","rb");
+
+    if (arch==NULL)
+    {
+        printf("\nEl archivo no fue creado o se elimino, contacte con soporte.");
+    }
+    else
+    {
+        fread(&reg_mascotas[i],sizeof(Mascota),1,arch);
+        while (!feof(arch))
+        {
+        i++;
+        fread(&reg_mascotas[i],sizeof(Mascota),1,arch); 
+        }
+        num_mascotas = i;
+        i = 0;
+        fclose(arch);
+    }
+    
+    //Archivo de turnos:
+    arch=fopen("Turnos.dat","rb");
+
+    if (arch==NULL)
+    {
+        printf("\nEl archivo no fue creado o se elimino, contacte con soporte.");
+    }
+    else
+    {
+        fread(&reg_turnos[i],sizeof(Turnos),1,arch);
+        while (!feof(arch))
+        {
+        i++;
+        fread(&reg_turnos[i],sizeof(Turnos),1,arch); 
+        }
+        num_turnos = i;
+        i = 0;
+        fclose(arch);
+    }
 }
 
 

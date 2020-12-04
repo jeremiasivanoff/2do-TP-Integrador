1039dc53a7df878826ed7c2b6579e921fb036125
JeremiasIvanoff <jeremias.morenoivanoff@alu.frt.utn.edu.ar>
2020-12-04 15:27:52 -0300

    Nueva condición if

--- Modulo de Administración.cpp
+++ Modulo de Administración.cpp
@@ -69,7 +69,7 @@ void registrarvet(FILE *vet1, veterinario vet)
 	scanf("%d",&vet.matri);
 	printf("\nIngrese su dni: ");
 	scanf("%d",&vet.dni);
-	printf("\nIngrese su número de teléfono: ");
+	printf("\nIngrese su nï¿½mero de telï¿½fono: ");
 	_flushall();
 	gets(vet.tel);
 	fwrite(&vet,sizeof(veterinario),1,vet1);
@@ -79,7 +79,7 @@ void registrarvet(FILE *vet1, veterinario vet)
 void registrarusuario(FILE *usuario1, usuario user)
 {
 	int i,v=0;
-	char letras[28]= "abcdefghijklmnñopqrstuvwxyz";
+	char letras[28]= "abcdefghijklmnï¿½opqrstuvwxyz";
 	system("CLS");
 	printf("\tIngrese los datos del usuario");
 	printf("\nNombre de Usuario: ");
@@ -87,20 +87,21 @@ void registrarusuario(FILE *usuario1, usuario user)
 	gets(user.usuario);
 	do
 	{
-		if(user.usuario[i]=='a' or user.usuario[i]=='b' or user.usuario[i]=='c' or user.usuario[i]=='d' or user.usuario[i]=='e' or user.usuario[i]=='f' or user.usuario[i]=='g' or user.usuario[i]=='h'or user.usuario[i]=='i'or user.usuario[i]=='o' or user.usuario[i]=='j' or user.usuario[i]=='k' or user.usuario[i]=='l' or user.usuario[i]=='m' or user.usuario[i]=='n' or user.usuario[i]=='ñ' or user.usuario[i]=='o' or user.usuario[i]=='p' or user.usuario[i]=='q' or user.usuario[i]=='r' or user.usuario[i]=='s' or user.usuario[i]=='t' or user.usuario[i]=='u' or user.usuario[i]=='v' or user.usuario[i]=='w' or user.usuario[i]=='x' or user.usuario[i]=='y' or user.usuario[i]=='z')
+		if(user.usuario[0]>96 and user.usuario[0]<123)
 		{
-			v++;
+			printf("\n La primera letra es minuscula.");
+			//v++;
 		}
 		i++;
-		if(v==0)
+		/*if(v==0)
 		{
-			printf("Error usuario no válido - El primer carácter debe ser minúscula Ingrese nuevamente.");
+			printf("Error usuario no vï¿½lido - El primer carï¿½cter debe ser minï¿½scula Ingrese nuevamente.");
 			system("pause");
 			system("CLS");
 			printf("\nNombre de Usuario: ");
 			_flushall();
 			gets(user.usuario);
-		}
+		}*/
 	}
 	while(strlen(user.usuario)>=6 and strlen(user.usuario)<=10);
 }
@@ -109,15 +110,15 @@ int menuprincipal()
 {
 	int opc=0;
 	system("CLS");
-	printf("\tMódulo Administración");
+	printf("\tMï¿½dulo Administraciï¿½n");
 	printf("\n======================");
 	printf("\n1.- Registrar Veterinario");
 	printf("\n2.- Registrar Usuario Asistente");
 	printf("\n3.- Atenciones por Veterinarios");
 	printf("\n4.- Ranking de Veterinarios por atenciones");
 	printf("\n5.- Registrar Veterinario");
-	printf("\n6.- Cerrar Aplicación");
-	printf("\nSeleccione una opción: ");
+	printf("\n6.- Cerrar Aplicaciï¿½n");
+	printf("\nSeleccione una opciï¿½n: ");
 	scanf("%d",&opc);
 	return opc;
 }
